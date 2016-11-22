029c6d11dee5208f52239de6fc1064d6e1802022
Chie <akiba.chie@gmail.com>
2016-11-22 12:14:49 -0800

    Added test script to package.json and also created a mock mocha test

--- test/create_test.js
+++ test/create_test.js
@@ -1,1 +1,8 @@
-describe();
+const assert = require('assert');
+
+describe('Creating records', () => { //use intuitive names for the string (what it is testing for)
+  it('saves a user', () => {
+    //Assertion - testing to see if your program is running properly
+    assert(1+1 === 2);
+  });
+});
