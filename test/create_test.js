78f7b241a2a75840eb025d23b9bbac1fe64c97cd
Chie <akiba.chie@gmail.com>
2016-11-22 12:45:23 -0800

    Added new user to mongodb

--- test/create_test.js
+++ test/create_test.js
@@ -1,8 +1,11 @@
 const assert = require('assert');
+const User = require('./../src/user')
 
 describe('Creating records', () => { //use intuitive names for the string (what it is testing for)
   it('saves a user', () => {
     //Assertion - testing to see if your program is running properly
-    assert(1+1 === 2);
+    const joe = new User({ name: 'Joe' });
+
+    joe.save(); //saves/insert this object into the databast
   });
 });
